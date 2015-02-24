2d1cebb0b5e4e6c7a1f80050e120f6c3348a9e0d
WMDE-Fisch <christoph.fischer@wikimedia.de>
2015-02-24 16:35:06 +0100

    tracking for links

--- suggest.js
+++ suggest.js
@@ -104,5 +104,5 @@ $( document ).ready( function() {
 
 function triggerPiwikTrack( element, code ) {
 	var piwikImgUrl = 'https://spenden.wikimedia.de/piwik/piwik.php?idsite=1&url=https://www.wikipedia.de/link-clicked/' + code + '&rec=1';
-	console.error($(element).prepend( '<img src="' + piwikImgUrl + '" width="0" height="0" border="0" />' ));
+	$(element).prepend( '<img src="' + piwikImgUrl + '" width="0" height="0" border="0" />' );
 }
