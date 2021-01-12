f3ddc836d24d3246ca8a96a897d4ab8685da6ae0
Abban Dunne <abban.dunne@wikimedia.de>
2021-01-12 13:33:38 +0100

    Add footer styles for Wikipedia anniversary

--- footer.php
+++ footer.php
@@ -1,25 +1,37 @@
 <footer class="page-footer" id="donationfooter">
-    <div class="link-block">
-        <ul>
-            <li><a onclick="triggerPiwikTrack(this, 'wikimedia.de-logo');" href="https://www.wikimedia.de"><img class="wikimedia-logo" src="img/wmde_logo_white.svg" alt="Wikimedia Deutschland e.V."></a></li>
-            <li>
-                <p><strong>Wikimedia Deutschland e. V.</strong></p>
-                <p><a href="https://www.wikimedia.de/de/ueber-uns">Über uns</a></p>
-                <p><a href="https://wikimedia-deutschland.softgarden.io/de/vacancies">Stellenangebote</a></p>
-                <p><a href="https://www.wikimedia.de/de/impressum">Impressum & Kontakt</a></p>
-            </li>
-            <li>
-                <p><strong>Mitwirken</strong></p>
-                <p><a href="https://spenden.wikimedia.de/apply-for-membership?piwik_campaign=wpdefooter&piwik_kwd=wpdefooterbtn">Mitglied werden</a></p>
-                <p><a href="https://spenden.wikimedia.de/?piwik_campaign=wpdefooter&piwik_kwd=wpdefooterbtn">Jetzt spenden</a></p>
-                <p><a href="https://spenden.wikimedia.de/use-of-funds?piwik_campaign=wpdefooter&piwik_kwd=wpdefooterbtn">Mittelverwendung</a></p>
-            </li>
-            <li>
-                <p><strong>Vereinskanäle</strong></p>
-                <p><a href="https://blog.wikimedia.de/">Unser Blog</a></p>
-                <p><a href="https://www.facebook.com/WMDEeV">Facebook</a></p>
-                <p><a href="https://twitter.com/wikimediade">Twitter</a></p>
-            </li>
-        </ul>
-    </div>
+	<div class="page-footer-column">
+		<a onclick="triggerPiwikTrack(this, 'wikipedia-20-logo');" href="https://www.wikimedia.de/wikipedia20" class="anniversary">
+			<img src="img/Wikipedia_20.png" alt="Wikipedia 20" class="anniversary-logo">
+			<span class="anniversary-cta">→ 20 Jahre Wikipedia</span>
+		</a>
+	</div>
+	<div class="page-footer-column">
+		<div class="link-block">
+			<ul>
+				<li>
+					<p class="wikimedia-logo-footer-container">
+						<a onclick="triggerPiwikTrack(this, 'wikimedia.de-logo');" href="https://www.wikimedia.de" class="wikimedia-logo-footer">
+							<img src="img/wmde_logo_footer.svg" alt="Wikimedia Deutschland e.V.">
+						</a>
+						<strong>Wikimedia Deutschland e. V.</strong>
+					</p>
+					<p><a href="https://www.wikimedia.de/de/ueber-uns">Über uns</a></p>
+					<p><a href="https://wikimedia-deutschland.softgarden.io/de/vacancies">Stellenangebote</a></p>
+					<p><a href="https://www.wikimedia.de/de/impressum">Impressum & Kontakt</a></p>
+				</li>
+				<li>
+					<p><strong>Mitwirken</strong></p>
+					<p><a href="https://spenden.wikimedia.de/apply-for-membership?piwik_campaign=wpdefooter&piwik_kwd=wpdefooterbtn">Mitglied werden</a></p>
+					<p><a href="https://spenden.wikimedia.de/?piwik_campaign=wpdefooter&piwik_kwd=wpdefooterbtn">Jetzt spenden</a></p>
+					<p><a href="https://spenden.wikimedia.de/use-of-funds?piwik_campaign=wpdefooter&piwik_kwd=wpdefooterbtn">Mittelverwendung</a></p>
+				</li>
+				<li>
+					<p><strong>Vereinskanäle</strong></p>
+					<p><a href="https://blog.wikimedia.de/">Unser Blog</a></p>
+					<p><a href="https://www.facebook.com/WMDEeV">Facebook</a></p>
+					<p><a href="https://twitter.com/wikimediade">Twitter</a></p>
+				</li>
+			</ul>
+		</div>
+	</div>
 </footer>
