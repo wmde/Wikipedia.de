9f13bb5f76db09645d7aaa78a67e379871e5fe64
Gabriel Birke <gabriel.birke@wikimedia.de>
2026-07-29 11:46:06 +0200

    Trigger suggest on input
    This will open the suggestions when the user pastes text in the input
    field or does other, non-keypress inputs.

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         <search id="maincontent" title="Wikipedia durchsuchen">
             <div id="form">
                 <form id="frmSearch" class="search-form" action="go.php" method="get" accept-charset="UTF-8">
-                    <input type="text" id="txtSearch" name="q" alt="Search Criteria" onkeyup="triggerSuggestLater('de')" autocomplete="off" aria-label="Wikipedia durchsuchen"/>
+                    <input type="text" id="txtSearch" name="q" alt="Search Criteria" oninput="triggerSuggestLater('de')" autocomplete="off" aria-label="Wikipedia durchsuchen"/>
                     <input type="hidden" name="l" value="de" />
                     <input type="hidden" name="e" value="wikipedia" /><input type="hidden" name="s" value="suchen" />
                     <button type="submit" id="cmdSearch" class="button-ooui-like" name="b">
